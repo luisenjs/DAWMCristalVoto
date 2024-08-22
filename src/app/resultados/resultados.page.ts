0a4fbbd9664bbcda975cf23895bb4605bb65f379
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2024-08-22 02:12:26 -0500

    update: estilos de pagina resultados

--- src/app/resultados/resultados.page.ts
+++ src/app/resultados/resultados.page.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonItem, IonAvatar, IonButton, IonNote, IonFooter, IonButtons, IonIcon, IonCardSubtitle, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonCard, IonCardTitle, IonCardHeader, IonCardContent, IonItem, IonAvatar, IonButton, IonNote, IonFooter, IonButtons, IonIcon, IonCardSubtitle, IonGrid, IonRow, IonCol, IonText } from '@ionic/angular/standalone';
 import { arrowBack } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { AlertController } from '@ionic/angular';
@@ -12,7 +12,7 @@ import { AlertController } from '@ionic/angular';
   templateUrl: './resultados.page.html',
   styleUrls: ['./resultados.page.scss'],
   standalone: true,
-  imports: [IonCol, IonRow, IonGrid, IonCardSubtitle, IonIcon, IonButtons, IonFooter, IonNote, IonButton, IonAvatar, IonItem, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonHeader, IonLabel, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonText, IonCol, IonRow, IonGrid, IonCardSubtitle, IonIcon, IonButtons, IonFooter, IonNote, IonButton, IonAvatar, IonItem, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonHeader, IonLabel, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ResultadosPage implements OnInit {
 
