edca9fae5110fc25653d5fee194e5072b5a7ca4a
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2024-08-22 23:55:51 -0500

    uodate: grafico creado

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,18 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardTitle, IonCardHeader, IonCardContent } from '@ionic/angular/standalone';
+import { ApexAxisChartSeries, ApexChart, ApexXAxis, ApexYAxis, ApexDataLabels, ApexPlotOptions, ApexTitleSubtitle, NgApexchartsModule, ApexTooltip } from 'ng-apexcharts';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardContent, IonCardHeader, IonCardTitle, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NgApexchartsModule]
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  public chartOptions: {
+    series: ApexAxisChartSeries;
+    chart: ApexChart;
+    xaxis: ApexXAxis;
+    yaxis: ApexYAxis;
+    dataLabels: ApexDataLabels;
+    plotOptions: ApexPlotOptions;
+    title: ApexTitleSubtitle;
+    tooltip: ApexTooltip;
+  };
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Primera Vuelta",
+          data: [75, 65, 35, 25]
+        },
+        {
+          name: "Segunda Vuelta",
+          data: [61, 49, 0, 0]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ["Daniel Noboa - ADN", "Luisa Gonzalez - RC", "Jan Topic - PSC", "Christian Zurita - MC25"],
+        labels: {
+          style: {
+            colors: ['#ffffff'],
+          }
+        }
+      },
+      yaxis: {
+        labels: {
+          style: {
+            colors: ['#ffffff'],
+          }
+        }
+      },
+      title: {
+        text: "Gráfico de Barras",
+        style: {
+          color: '#ffffff'
+        }
+      },
+      tooltip: {
+        theme: 'dark',
+      }
+    }
+  };
 
   ngOnInit() {
   }
