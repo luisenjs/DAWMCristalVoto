b5288034724a1ed9ffae59a7718f69ab36c51b8d
Julio Guerrero Alvarez <judaguer@espol.edu.ec>
2024-09-20 08:09:33 -0500

    variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBy7tWJWC80A_ZJU7v2AxaJ4xxBeeM2tCA",
+    authDomain: "votaciones-3a6b7.firebaseapp.com",
+    databaseURL: "https://votaciones-3a6b7-default-rtdb.firebaseio.com",
+    projectId: "votaciones-3a6b7",
+    storageBucket: "votaciones-3a6b7.appspot.com",
+    messagingSenderId: "957230168508",
+    appId: "1:957230168508:web:1ab1bb9d0d226649b213b9"
+  }
 };
